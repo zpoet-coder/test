0dd145cf6a16b2deb944fd92756f200006b6fef0
zpoet-coder <zpoet.personal@gmail.com>
2024-07-11 23:50:25 +0800

    feat: add click functionality to adapt to mobile devices

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-import { playSound, removeClassName } from "/src/function.js";
+import { playSound, upKeyOrMouse } from "/src/function.js";
 
+// 添加键盘按下和松开事件
 window.addEventListener('keydown', playSound);
-window.addEventListener('keyup', removeClassName);
+window.addEventListener('keyup', upKeyOrMouse);
+
+// 添加鼠标按下和松开事件
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+  key.addEventListener('mousedown', playSound);
+  key.addEventListener('mouseup', upKeyOrMouse);
+});
+
+// 当点击span时，找到最近的key元素并触发点击事件
+const spans = document.querySelectorAll('.key span');
+spans.forEach(span => {
+  span.addEventListener('click', function (event) {
+    const keyElement = span.closest('.key');
+    if (keyElement) {
+      keyElement.querySelector('kbd').click();
+    }
+  });
+});
--- src/function.js
+++ src/function.js
@@ -1,17 +1,55 @@
+/* 
+移除playing类名
+ */
+function removeClassName(asciiCode) {
+  const key = document.querySelector(`div[data-key="${asciiCode}"]`);
+  if (!key) return;
+  key.classList.remove("playing");
+}
+
+/* 
+根据键盘事件或鼠标事件获取ascii码
+通过data-key属性定位元素
+播放音频并添加playing类名
+*/
 export function playSound(event) {
-  // console.log(event);
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  var asciiCode;
+  // 分别处理键盘事件和鼠标事件
+  if (event.type === 'keydown') {
+    asciiCode = event.keyCode;
+  } else if (event.type === 'mousedown' || event.type === 'click') {
+    const keyElement = event.target.closest('.key');
+    if (!keyElement) return;
+    asciiCode = parseInt(keyElement.getAttribute('data-key'));
+  }
+
+
+  // 定位要播放音频和添加类名的元素
+  const audio = document.querySelector(`audio[data-key="${asciiCode}"]`);
   if (!audio) return;
-  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-  // console.log('audioElement', audio, '\nkeyElement', key);
+  const key = document.querySelector(`div[data-key="${asciiCode}"]`);
 
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
 }
 
-export function removeClassName(event) {
-  // console.log(event);
-  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-  key.classList.remove("playing");
+/* 
+根据键盘事件或鼠标事件获取ascii码
+通过data-key属性定位元素
+移除playing类名
+*/
+export function upKeyOrMouse(event) {
+  var asciiCode;
+  if (event.type === 'keyup') {
+    asciiCode = event.keyCode;
+    removeClassName(asciiCode);
+  }
+  else if (event.type === 'mouseup') {
+    const keyElement = event.target.closest('.key');
+    if (!keyElement) return;
+    asciiCode = parseInt(keyElement.getAttribute('data-key'));
+
+    setTimeout(() => removeClassName(asciiCode), 70)
+  }
 }
