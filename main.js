15988c192f7e0d012c4f695349378ad67aadbdd1
zpoet-coder <zpoet.personal@gmail.com>
2024-07-12 16:49:12 +0800

    complete exercise 02 - JS and CSS Clock

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-import { playSound, upKeyOrMouse } from "/src/function.js";
+// 获取时、分、秒三个指针元素
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-// 添加键盘按下和松开事件
-window.addEventListener('keydown', playSound);
-window.addEventListener('keyup', upKeyOrMouse);
+/*
+* 根据当前时间计算时、分、秒三个指针的旋转角度
+* 每秒刷新一次
+*/
+function setDateTime() {
+  const now = new Date();
+  // 在页面显示当前时间
+  const currentTime = document.querySelector('.current-time span');
+  currentTime.textContent = now;
+  currentTime.style.color = '#ccc';
 
-// 添加鼠标按下和松开事件
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => {
-  key.addEventListener('mousedown', playSound);
-  key.addEventListener('mouseup', upKeyOrMouse);
-});
+  const seconds = now.getSeconds();
+  const secondsDeg = (seconds / 60) * 360;
+  secondHand.style.rotate = `${secondsDeg}deg`;
 
-// 当点击span时，找到最近的key元素并触发点击事件
-const spans = document.querySelectorAll('.key span');
-spans.forEach(span => {
-  span.addEventListener('click', function (event) {
-    const keyElement = span.closest('.key');
-    if (keyElement) {
-      keyElement.querySelector('kbd').click();
-    }
-  });
-});
+  const mins = now.getMinutes();
+  const minsDeg = (mins / 60) * 360 + (seconds / 60) * 6;
+  minHand.style.rotate = `${minsDeg}deg`;
+
+  const hours = now.getHours();
+  const hoursDeg = (hours / 12) * 360 + (mins / 60) * 30;
+  hourHand.style.rotate = `${hoursDeg}deg`;
+}
+
+// 初始化时间，并且每秒更新一次
+setDateTime();
+setInterval(setDateTime, 1000);
